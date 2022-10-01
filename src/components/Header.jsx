3b38a29a7f481d1c031578f665c36c22d3d487ba
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-01 13:49:48 +0300

    Добавил при клике на header-logo роутинг на главную

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
+        <Link to="/" className="header__logo">
           <img width="38" src={pizzaLogo} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
           </div>
-        </div>
+        </Link>
         <div className="header__cart">
           <Link to="cart" className="button button--cart">
             <span>520 ₽</span>
