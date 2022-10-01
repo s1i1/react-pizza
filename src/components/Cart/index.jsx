c7e3d9f5f9230d22e543464868815e7a2fa34f5d
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-01 13:34:58 +0300

    Добавил в Cart - Link to='/'

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
   return (
@@ -377,7 +378,7 @@ const Cart = () => {
           </span>
         </div>
         <div class="cart__bottom-buttons">
-          <a href="/" class="button button--outline button--add go-back-btn">
+          <Link to="/" class="button button--outline button--add go-back-btn">
             <svg
               width="8"
               height="14"
@@ -394,7 +395,7 @@ const Cart = () => {
             </svg>
 
             <span>Вернуться назад</span>
-          </a>
+          </Link>
           <div class="button pay-btn">
             <span>Оплатить сейчас</span>
           </div>
