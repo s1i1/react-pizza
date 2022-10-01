e3a49c90a3f80c3bc52c5e92fbb7cf3e62cba01c
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-01 14:24:44 +0300

    Сверстал NotFound

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import NotFoundPage from '../../assets/img/404-error.png';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="cart cart--empty">
+      <img
+        src={NotFoundPage}
+        alt="NotFoundPage"
+        style={{ marginTop: '0px', marginBottom: '0px' }}
+      />
+      <h2>
+        Страница не найдена <icon>😕</icon>
+      </h2>
+      <p>
+        Вернитесь на главную страницу
+        <br />
+        Или подождите 3 секунды и вас автоматически перенаправит на главную страницу
+      </p>
+      <Link to="/" className="button button--black" style={{ marginTop: '40px' }}>
+        <span>Вернуться назад</span>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
