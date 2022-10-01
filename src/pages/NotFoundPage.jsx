a6db5260289b7487b59afecbe444808889683283
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-01 14:59:54 +0300

    Сделал в NotFoundPage - Перенаправление на главную через 3 секунды

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import NotFound from '../components/NotFound';
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 3000);
+  });
+
   return <NotFound />;
 };
 
