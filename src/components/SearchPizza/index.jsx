cd0c760874ae22a7297c5adf4602622571186c80
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-09 14:24:12 +0300

    В SearchPizza сделал debounce для инпута

--- src/components/SearchPizza/index.jsx
+++ src/components/SearchPizza/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 import searchIcon from '../../assets/img/search-icon.svg';
 import clearSearch from '../../assets/img/delete-icon.svg';
 import SearchContext from '../context/SearchContext';
 import styles from './SearchPizza.module.scss';
 
 const SearchPizza = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const { setSearchValue } = React.useContext(SearchContext);
+
+  const [value, setValue] = React.useState('');
+
   const searchInputRef = React.useRef();
 
   const onClearSearch = () => {
     setSearchValue('');
+    setValue('');
     searchInputRef.current.focus();
+  };
+
+  const inputDebounce = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 250),
+    [],
+  );
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+
+    inputDebounce(e.target.value);
   };
 
   return (
@@ -19,11 +37,11 @@ const SearchPizza = () => {
         ref={searchInputRef}
         className={styles.input}
         placeholder="Поиск пиццы..."
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        value={value}
+        onChange={(e) => onChangeInput(e)}
       />
       <img className={styles.search} src={searchIcon} alt="searchIcon" />
-      {searchValue && (
+      {value && (
         <img className={styles.clear} src={clearSearch} alt="clearSearch" onClick={onClearSearch} />
       )}
     </div>
