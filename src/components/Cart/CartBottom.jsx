cd3b73b07ad3a90fb0b3a3827846fc0acf438325
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-10 16:39:31 +0300

    В Cart вынес CartBottom в отдельный компонент

---
+++ src/components/Cart/CartBottom.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CartBottom = () => {
+  return (
+    <div className="cart__bottom">
+      <div className="cart__bottom-details">
+        <span>
+          {' '}
+          Всего пицц: <b>3 шт.</b>{' '}
+        </span>
+        <span>
+          {' '}
+          Сумма заказа: <b>900 ₽</b>{' '}
+        </span>
+      </div>
+      <div className="cart__bottom-buttons">
+        <Link to="/" className="button button--outline button--add go-back-btn">
+          <svg
+            width="8"
+            height="14"
+            viewBox="0 0 8 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M7 13L1 6.93015L6.86175 1"
+              stroke="#D3D3D3"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+
+          <span>Вернуться назад</span>
+        </Link>
+        <div className="button pay-btn">
+          <span>Оплатить сейчас</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartBottom;
