d45d9eda685a7d35e516aa8df47df6879a6ac0bd
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-11 15:38:33 +0300

    В CartPage сделал отрисовку пицц cartItems

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import CartTop from '../components/Cart/CartTop';
 import CartItem from '../components/Cart/CartItem';
 import CartBottom from '../components/Cart/CartBottom';
 
 const CartPage = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <div className="container container--cart">
       <div className="cart">
         <CartTop />
-        <CartItem />
+        {cartItems.map((obj) => {
+          return <CartItem key={obj.id} {...obj} />;
+        })}
         <CartBottom />
       </div>
     </div>
