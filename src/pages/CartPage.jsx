71126c52dfcb69c369794bb1580c4891565be417
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-11 17:45:21 +0300

    Сделал отрисовку EmptyCard если cartItems пустой

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Cart from '../components/Cart/index';
+import EmptyCart from '../components/Cart/EmptyCart';
 
 const CartPage = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <div className="container container--cart">
-      <Cart />
+      {cartItems.length > 0 ? <Cart /> : <EmptyCart />}
     </div>
   );
 };
