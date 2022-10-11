633a0a3bd159699bddf8fd239d474655ec7c88f2
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-11 15:48:32 +0300

    Revert "В cartSlice переименовал total на totalPrice"
    This reverts commit cfc5bcbb4b753c7251f54bac4fdbab9d122a0690.

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const CartItem = ({ count, currentSize, currentType, id, imageUrl, name, price }) => {
+const CartItem = () => {
   return (
     <div className="cart__item">
       <div className="cart__item-img">
-        <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+        <img
+          className="pizza-block__image"
+          src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+          alt="Pizza"
+        />
       </div>
       <div className="cart__item-info">
-        <h3>{name}</h3>
-        <p>
-          {currentType} тесто, {currentSize} см.
-        </p>
+        <h3>Сырный цыпленок</h3>
+        <p>тонкое тесто, 26 см.</p>
       </div>
       <div className="cart__item-count">
         <div className="button button--outline button--circle cart__item-count-minus">
@@ -30,7 +32,7 @@ const CartItem = ({ count, currentSize, currentType, id, imageUrl, name, price }
             />
           </svg>
         </div>
-        <b>{count}</b>
+        <b>2</b>
         <div className="button button--outline button--circle cart__item-count-plus">
           <svg
             width="10"
@@ -50,7 +52,7 @@ const CartItem = ({ count, currentSize, currentType, id, imageUrl, name, price }
         </div>
       </div>
       <div className="cart__item-price">
-        <b>{price} ₽</b>
+        <b>770 ₽</b>
       </div>
       <div className="cart__item-remove">
         <div className="button button--outline button--circle">
