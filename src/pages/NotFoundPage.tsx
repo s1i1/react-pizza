cb5d0224c1b8a2a425ce77a4be26e7a22ff8f01f
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-17 12:49:52 +0300

    Добавил React.FC в NotFoundPage,Header

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import NotFound from '../components/NotFound';
 
-const NotFoundPage = () => {
+const NotFoundPage: React.FC = () => {
   const navigate = useNavigate();
 
   React.useEffect(() => {
