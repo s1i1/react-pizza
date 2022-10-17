ed3aa49217b4638d8818a53d61ff47c189cb98f5
Some1 <111959143+s1i1@users.noreply.github.com>
2022-10-17 14:23:07 +0300

    Типизировал store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -16,3 +16,6 @@ export const store = configureStore({
     cart: cartSlice,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
